308ba9afdc68bb811daa642679c41867114e535d
Miracle pininee siddert Saturday <110922869+miraclesiddert@users.noreply.github.com>
2025-08-10 00:54:29 +0100

    Adding the the first update of the app

---
+++ Pizza-Menu/src/App.jsx
@@ -0,0 +1,24 @@
+// import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>
+        
+      </div>
+      <h1>Pizza Menu</h1>
+      <div className="card">
+        <h2>Our Pizzas</h2>
+        <ul>
+          <li>Margherita</li>
+          <li>Pepperoni</li>
+          <li>BBQ Chicken</li>
+          <li>Veggie</li>
+        </ul>
+      </div>
+    </>
+  )
+}
+
+export default App
